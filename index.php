ee10f5a295a367d07d0eada648f295e010409e5d
Miles Dickinson <miles.dickinson92@gmail.com>
2016-01-12 11:34:30 -0500

    Add index.php page with form and create and link main.js

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            JS-PHP-PYTHON AJAX Call
+        </title>
+    </head>
+    <body>
+       <form id="ajax-call">
+            <label for="name">Name</label>
+            <input name="name" id="name">
+            <button id="submit-ajax-form">Submit</button>
+       </form>
+       <script src="./main.js"></script>
+    </body>
+</html>
