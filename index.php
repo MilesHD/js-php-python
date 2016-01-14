8dd9182ae502242768f17e523ad36a8a39890d55
Miles Dickinson <miles.dickinson92@gmail.com>
2016-01-14 15:29:07 -0500

    Add application UI structure and styling

--- index.php
+++ index.php
@@ -5,8 +5,46 @@
             JS-PHP-PYTHON AJAX Call
         </title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/vis/4.12.0/vis.min.css">
+        <link rel="stylesheet" href="./style.css">
     </head>
     <body>
+       <div class="medusa">
+            <div class="medusaMenu">
+                <div class="medusaMenuHeader">
+                   <img src="http://placehold.it/200x100"> 
+                </div>
+                <div class="medusaMenuValidationError">
+                   <div class="medusaError">Please enter a valid reference</div> 
+                   <div class="medusaError">Please enter a number of days back</div> 
+                   <div class="medusaError">Please select at least one source</div> 
+                </div>
+                <div class="medusaMenuControls">
+                   <form id="MedusaQuery">
+                        <label for="reference"></label>
+                        <input type="text" name="reference" id="Reference" 
+                        class="medusaMenuControlsInput" placeholder="Reference">
+
+                        <label for="daysBack"></label>
+                        <input type="number" name="daysBack" id="DaysBack" 
+                        class="medusaMenuControlsInput" placeholder="DaysBack">
+
+                        <div class="medusaMenuControlsSources">
+                            Sources
+                            <input type="checkbox" name="sources" value="DISE"> DSIE
+                            <input type="checkbox" name="sources" value="GE"> GE 
+                        </div>
+
+                        <div>
+                            <button id="SubmitQuery" class="medusaMenuButton medusaMenuButtonSubmit">Submit</button>
+                            <button id="CleaerForm" class="medusaMenuButton medusaMenuButtonClear">Clear</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div id="MedusaVis" class="medusaVis">
+            </div>
+       </div>
+        <!--
         <form id="ajax-call">
             <label for="name">Name</label>
             <input name="name" id="name">
@@ -17,6 +55,7 @@
         </div>
         <div id="visualization">
         </div>
+        -->
         <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/vis/4.12.0/vis.min.js"></script>
         <script src="./main.js"></script>
