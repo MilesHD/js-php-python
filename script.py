6503f9ad37acdaefdf1996175f758feac9a1297c
Miles Dickinson <miles.dickinson92@gmail.com>
2016-01-20 15:21:08 -0500

    Change first query to retrieve indicator document that matches passed in indicator _id

--- script.py
+++ script.py
@@ -14,6 +14,15 @@ class JSONEncoder(json.JSONEncoder):
             return str(o)
         return json.JSONEncoder.default(self, o)
 
+# Attempt to retrieve arguments passed in with program call
+try:
+    indicator_id = sys.argv[1]
+    # days_back = sys.argv[2]
+    # sources = sys.argv[3]
+except IndexError:
+    print "Medusa was not called with the proper arguments. Please check your input and try again"
+    sys.exit(0)
+
 # Connect to Local MongoDB Instance
 client = MongoClient()
 db = client.test
@@ -22,7 +31,7 @@ db = client.test
 
 indicators = []
 for ind in db.indicators.find(
-        filter={}, 
+        filter={"_id": indicator_id}, 
         projection={
             "value": 1,
             "status": 1,
@@ -31,38 +40,40 @@ for ind in db.indicators.find(
             "source.instances.reference": 1}):
     indicators.append(ind)
 
+print indicators
+
 ########### DATA TRANSFORMATION ##########
 
-nodes = []
-edges = []
-references = []
+# nodes = []
+# edges = []
+# references = []
 
-# Extract References and create nodes
-for ind in indicators:
-    for src in ind["source"]:
-        for inst in src["instances"]:
-            references.append(inst["reference"])
-            node = {
-                "id": str(ind["_id"]),
-                "value": ind["value"],
-                "reference": inst["reference"]
-            }
-            nodes.append(node)
+# # Extract References and create nodes
+# for ind in indicators:
+    # for src in ind["source"]:
+        # for inst in src["instances"]:
+            # references.append(inst["reference"])
+            # node = {
+                # "id": str(ind["_id"]),
+                # "value": ind["value"],
+                # "reference": inst["reference"]
+            # }
+            # nodes.append(node)
 
-# Create Edges
-for ind in indicators:
-    for src in ind["source"]:
-        for inst in src["instances"]:
-            for ref in references:
-                if "reference" in inst and inst["reference"] in references and {"from": str(ind["_id"]), "to": ref} not in edges:
-                    edges.append({"from": str(ind["_id"]), "to": ref})
+# # Create Edges
+# for ind in indicators:
+    # for src in ind["source"]:
+        # for inst in src["instances"]:
+            # for ref in references:
+                # if "reference" in inst and inst["reference"] in references and {"from": str(ind["_id"]), "to": ref} not in edges:
+                    # edges.append({"from": str(ind["_id"]), "to": ref})
 
 
-nodes = JSONEncoder().encode(nodes)
-edges = JSONEncoder().encode(edges)
+# nodes = JSONEncoder().encode(nodes)
+# edges = JSONEncoder().encode(edges)
 
 ########### DATA TRANSMISSION ##########
 
 # print is a thin wrapper that formats inputs and
 # calls the write function on a given object (default: sys.stdout)
-print nodes + "SPLIT" + edges
+# print nodes + "SPLIT" + edges
